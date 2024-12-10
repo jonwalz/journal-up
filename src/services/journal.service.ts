ccc4c43c0009b39d35a2b6604e10412429211543
jonwalz <jon.walz.music@gmail.com>
2024-12-10 22:02:09 +0100

    debug zep

--- src/services/journal.service.ts
+++ src/services/journal.service.ts
@@ -4,6 +4,8 @@ import type { IJournal, IEntry, IGrowthIndicators } from "../types";
 import { ZepClient } from "@getzep/zep-cloud";
 import { env } from "../config/environment";
 
+console.log("Zep API key:", env.ZEP_API_KEY);
+
 interface ZepMemory {
   message?: {
     content: string;
